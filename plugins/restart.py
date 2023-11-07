d8a71110ba181dcc86be8486c83762a0edea0acd
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2023-11-07 18:41:33 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -3,8 +3,8 @@ from info import ADMINS
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command("restart") & filters.user(ADMINS))
-async def stop_button(bot, message):
-    msg = await message.reply(text="♨️")       
-    await asyncio.sleep(3)
-    await msg.edit("✅")
+async def restart_bot(bot, msg):
+    await msg.reply("Rᴇꜱᴛᴀᴛɪɴɢ........")
+    await asyncio.sleep(2)
+    await sts.delete()
     os.execl(sys.executable, sys.executable, *sys.argv)
