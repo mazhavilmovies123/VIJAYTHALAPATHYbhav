8751076d65500a9e3044a00de92d4b6b1b8e2ec7
mazhavilmovies123 <muralirokz35@gmail.com>
2025-09-14 10:47:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,26 +17,26 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '7049796'))
-API_HASH = environ.get('API_HASH', '1db4ace85d66d7c298fc7542359cc8f2')
-BOT_TOKEN = environ.get('BOT_TOKEN', '2145285398:AAHqLNrJRPcIUm3rMEeN___4z6bd1a2IVU8')
+API_ID = int(environ.get('API_ID', '21419016'))
+API_HASH = environ.get('API_HASH', '79198e1eb4cfd0f771a89d83b9144e7e')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8337068928:AAFAvGMv0tTqDOMBFN8AKwuZc742WQMAKaw')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS' ,'http://telegra.ph/file/c54f4c4e8c05597210d43.jpg http://telegra.ph/file/dd8df1b6f794bdde4f8d3.jpg http://telegra.ph/file/64750a45d500d5cd97683.jpg http://telegra.ph/file/e7a7bb0b1d8c13c3feb1a.jpg http://telegra.ph/file/6f1d13ac1d52f9a23be49.jpg http://telegra.ph/file/14b6933c80b54997f56d5.jpg http://telegra.ph/file/157c18c056c9343b68462.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '683891378').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001793383615 -1002038316280').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1933114137').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002653541754').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', 'c4christy')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', 'mazhavil_bots')
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://bhavana:bhavana@cluster0.tybfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://melodysotto4_db_user:BCUKIKDEAqFEzeCj@cluster0.trrt89o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
@@ -50,8 +50,8 @@ REQ_CHANNEL = (int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001733637794'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'c4christy')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002957056021'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mazhavil_bots')
 MAX_B_TN = environ.get("MAX_B_TN", "8")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "False")), False)
 AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
